9fc9027139b6124ed9b0d71e760f97a8eec665bf
Siddhesh Surve <sysurve24@gmail.com>
2023-05-17 17:11:52 +0000

    implemented queue

--- random-practice/bfsdfs.cpp
+++ random-practice/bfsdfs.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 #define SIZE 1000
 
+using namespace std;
+
 class queue {
     int *arr;
     int capacity;
@@ -20,14 +22,56 @@ public :
     bool isFull();
 };
 
-queue :: queue {
+queue :: queue(int size) {
     arr = new int[size];
     capacity = size;
     front = 0;
     rear = -1;
-    capacity = 0;
+    count = 0;
 }
 
-int main () {
-
+queue :: ~queue() {
+    delete[] arr;
 }
+
+int queue :: pop() {
+    if(isEmpty()){
+        cout<<"Underflow\n";
+        exit(EXIT_FAILURE);
+    }
+    int x = arr[front];
+    front = (front+1)%capacity;
+    count--;
+    return x;
+}
+
+void queue :: push(int item) {
+    if(isFull()) {
+        cout<<"Overflow\n";
+        exit(EXIT_FAILURE);
+    }
+    rear = (rear+1)%capacity;
+    arr[rear] = item;
+    count++;
+}
+
+int queue::peek() {
+    if(isEmpty()){
+        cout<<"Underflow\n";
+        exit(EXIT_FAILURE);
+    }
+    return arr[front];
+}
+
+int queue::size(){
+    return count;
+}
+
+bool queue::isEmpty() {
+    return (size() == 0);
+}
+
+bool queue::isFull() {
+    return (size() == capacity);
+}
+
